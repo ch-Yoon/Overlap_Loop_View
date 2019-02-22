8e2a2034df5a3606617b5797374c905e75ed1167
ch-Yoon <chanhyuck1021@naver.com>
2019-02-22 17:27:30 +0900

    Add sample project

---
+++ Sample/app/src/main/java/com/view/loop/overlap/why/yoon/ch/sample/SampleItem.java
@@ -0,0 +1,15 @@
+package com.view.loop.overlap.why.yoon.ch.sample;
+
+public class SampleItem {
+
+    private int backgroundColor;
+
+    public SampleItem(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+}
---
+++ Sample/app/src/main/java/com/view/loop/overlap/why/yoon/ch/sample/SampleViewHolder.java
@@ -0,0 +1,27 @@
+package com.view.loop.overlap.why.yoon.ch.sample;
+
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.view.loop.overlap.why.yoon.ch.overlaploopviewlib.OverlapLoopView;
+
+public class SampleViewHolder extends OverlapLoopView.ViewHolder {
+
+    private ImageView sampleImageView;
+    private TextView sampleTextView;
+
+    public SampleViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        sampleImageView = itemView.findViewById(R.id.sampleImageView);
+        sampleTextView = itemView.findViewById(R.id.sampleTextView);
+    }
+
+    public void setItem(@NonNull SampleItem sampleItem) {
+        sampleImageView.setBackgroundColor(sampleItem.getBackgroundColor());
+        sampleTextView.setText(String.valueOf(getItemPosition()));
+    }
+
+}
