094de2ab117dee83ff27eaa836b89447b7a4a4f5
ch-Yoon <chanhyuck1021@naver.com>
2019-02-23 16:46:29 +0900

    Create sample code

---
+++ OverlapLoopView/app/src/main/java/com/view/loop/overlap/why/yoon/ch/overlaploopview/SampleItem.java
@@ -0,0 +1,15 @@
+package com.view.loop.overlap.why.yoon.ch.overlaploopview;
+
+public class SampleItem {
+
+    private int backgroundColor;
+
+    public SampleItem(int backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public int getBackgroundColor() {
+        return backgroundColor;
+    }
+
+}
